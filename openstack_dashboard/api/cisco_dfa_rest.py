b4770e373be5d8f72d73cad895efe286312b7d31
nlahouti <nlahouti@cisco.com>
2013-11-17 19:30:42 -0800

    Added copyright header.

--- openstack_dashboard/api/cisco_dfa_rest.py
+++ openstack_dashboard/api/cisco_dfa_rest.py
@@ -1,3 +1,23 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2013 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+#
+# Copyright 2013 Cisco Systems, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from __future__ import absolute_import
 import logging
 import ConfigParser
@@ -33,8 +53,8 @@ class DFARESTClient(object):
         return self._gw_mac
 
     def create_network(self, network_info):
-        url = 'http://%s/rest/auto-config/organizations/%s/partitions/%s/networks' % (self._ip, network_info['partitionName'], 
-              network_info['partitionName'])
+        url = 'http://%s/rest/auto-config/organizations/%s/partitions/%s/networks' \
+            % (self._ip, network_info['partitionName'], network_info['partitionName'])
         payload = network_info
 
         LOG.debug('url = {0}\npayload={1}'.format(url, payload))
